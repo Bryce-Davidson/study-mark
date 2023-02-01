32e1ac4b50051f6dd65273c5a4c68c5c4da25cad
Bryce-Davidson <lifebryce@gmail.com>
2023-02-01 10:40:09 -0800

    Added post page

--- src/pages/sessions/new.tsx
+++ src/pages/sessions/new.tsx
@@ -1,10 +1,39 @@
 import Layout from "@/components/layouts/Layout";
+import { useState } from "react";
 import { supabase } from "@/lib/supaBaseClient";
+import { useUser } from "@supabase/auth-helpers-react";
+
+interface StudySessionRow {
+  study_area_id: string;
+  profile_id: string;
+  class: string;
+  duration: string;
+  available_seats: number;
+}
 
 export default function Home() {
+  const user = useUser();
+  const [session, setSession] = useState<null | StudySessionRow>();
+
+  async function handlePostSession(user_id: undefined | string) {
+    console.log(user);
+    const { error } = await supabase.from("study_sessions").insert({
+      study_area_id: 0,
+      profile_id: user_id,
+      class: "320",
+      available_seats: 0,
+      duration: "2023-01-31 21:20:00+00",
+    });
+    console.log(error);
+  }
   return (
     <Layout>
-      <div>New Session</div>
+      <button
+        className="shadow-md p-10 mx-auto"
+        onClick={() => handlePostSession(user?.id)}
+      >
+        Post Session
+      </button>
     </Layout>
   );
 }
