986ce1c95a53488a9790e498c7654447cae9a672
Bryce-Davidson <lifebryce@gmail.com>
2023-02-02 14:32:22 -0800

    Added forms

--- src/components/SessionForm.tsx
+++ src/components/SessionForm.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import SelectSearch from "react-select-search";
 
 interface NewStudySession {
   study_area_id: string | undefined;
@@ -7,6 +8,16 @@ interface NewStudySession {
   expires_at: string | undefined;
   available_seats: number | undefined;
 }
+
+const options = [
+  { name: "Swedish", value: "sv" },
+  { name: "English", value: "en" },
+  {
+    type: "group",
+    name: "Group name",
+    items: [{ name: "Spanish", value: "es" }],
+  },
+];
 
 export default function StatusUpdateButton() {
   const [expiresAt, setExpiresAt] = useState<any>("");
@@ -28,13 +39,18 @@ export default function StatusUpdateButton() {
     console.log(session);
   };
   return (
-    <div className="flex justify-center mt-10">
+    <div className="flex flex-col items-center gap-5 justify-center mt-10">
       <input
-        className="timepicker border-2 rounded-lg text-xl px-20 py-5"
+        className="timepicker border-2 rounded-lg text-lg w-full py-3"
         onChange={handleExpiresAtchange}
         value={expiresAt}
         type="time"
         id="time-picker"
+      />
+      <SelectSearch
+        options={options}
+        search={true}
+        placeholder="Search for class"
       />
     </div>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider, Session } from "@supabase/auth-helpers-react";
 import { useState } from "react";
+import "../styles/react-select.css";
 
 export default function App({
   Component,
