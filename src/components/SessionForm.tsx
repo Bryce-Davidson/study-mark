12bbe190fc91a5edbdc5773c5ebe2af0ddd26deb
Bryce-Davidson <lifebryce@gmail.com>
2023-02-04 17:44:37 -0800

    Working time picker

--- src/components/SessionForm.tsx
+++ src/components/SessionForm.tsx
@@ -3,6 +3,7 @@ import { useUser } from "@supabase/auth-helpers-react";
 import { Button, TextField, Autocomplete } from "@mui/material";
 import { object, string } from "yup";
 import { useFormik } from "formik";
+import { TimePicker } from "@mui/x-date-pickers";
 
 const options = courses.course_ids;
 
@@ -15,6 +16,7 @@ export default function StatusUpdateButton() {
   const formik = useFormik({
     initialValues: {
       course: { label: "", id: 0 },
+      time: null,
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
@@ -40,6 +42,15 @@ export default function StatusUpdateButton() {
           }
           inputValue={formik.values.course.label}
           renderInput={(params) => <TextField {...params} label="Course" />}
+        />
+        <TimePicker
+          className="w-full"
+          label="Time"
+          value={formik.values.time}
+          onChange={(time) => {
+            formik.setFieldValue("time", time);
+          }}
+          renderInput={(params) => <TextField {...params} />}
         />
         <Button
           className="bg-blue-600 text-white focus:text-blue-600 hover:text-blue-600"
