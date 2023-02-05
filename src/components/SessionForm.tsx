8bc4730b5285b4dbd102074af4b4e8b868831814
Bryce-Davidson <lifebryce@gmail.com>
2023-02-04 17:34:39 -0800

    Added form validation

--- src/components/SessionForm.tsx
+++ src/components/SessionForm.tsx
@@ -1,5 +1,4 @@
 import courses from "../data/course_ids.json";
-import Fuse from "fuse.js";
 import { useUser } from "@supabase/auth-helpers-react";
 import { Button, TextField, Autocomplete } from "@mui/material";
 import { object, string } from "yup";
@@ -8,20 +7,14 @@ import { useFormik } from "formik";
 const options = courses.course_ids;
 
 const validationSchema = object({
-  email: string().email("Enter a valid email"),
-  password: string().min(
-    8,
-    "Password should be of minimum 8 characters length"
-  ),
+  course: object(),
 });
 
 export default function StatusUpdateButton() {
   const user = useUser();
   const formik = useFormik({
     initialValues: {
-      email: "",
-      password: "",
-      movie: { label: "", id: 0 },
+      course: { label: "", id: 0 },
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
@@ -31,39 +24,22 @@ export default function StatusUpdateButton() {
 
   return (
     <div>
-      <form className="flex flex-col gap-5" onSubmit={formik.handleSubmit}>
+      <form
+        className="flex flex-col gap-5 mt-10"
+        onSubmit={formik.handleSubmit}
+      >
         <Autocomplete
           options={options}
           getOptionLabel={(option) => option.label}
-          value={formik.values.movie}
-          isOptionEqualToValue={(option, value) =>
-            option.label === value.label || value.label == ""
+          value={formik.values.course}
+          isOptionEqualToValue={(option, value) => option.id === value.id}
+          onChange={(event, value) =>
+            value
+              ? formik.setFieldValue("course", value)
+              : formik.setFieldValue("course", { label: "", id: 0 })
           }
-          onChange={(event, value) => formik.setFieldValue("movie", value)}
-          //   inputValue={formik.values.movie.label}
-          //   onInputChange={formik.handleChange}
-          renderInput={(params) => <TextField {...params} label="Movie" />}
-        />
-        <TextField
-          fullWidth
-          id="email"
-          name="email"
-          label="Email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          error={formik.touched.email && Boolean(formik.errors.email)}
-          helperText={formik.touched.email && formik.errors.email}
-        />
-        <TextField
-          fullWidth
-          id="password"
-          name="password"
-          label="Password"
-          type="password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          error={formik.touched.password && Boolean(formik.errors.password)}
-          helperText={formik.touched.password && formik.errors.password}
+          inputValue={formik.values.course.label}
+          renderInput={(params) => <TextField {...params} label="Course" />}
         />
         <Button
           className="bg-blue-600 text-white focus:text-blue-600 hover:text-blue-600"
@@ -85,21 +61,3 @@ interface NewStudySession {
   expires_at: string | null | undefined;
   available_seats: string | undefined;
 }
-
-const fuseOptions = {
-  keys: ["label"],
-};
-const fuse = new Fuse(options, fuseOptions);
-
-const filterOptions = (
-  options: { label: string; id: number }[],
-  { inputValue }: { inputValue: any }
-) => {
-  const results = fuse.search(inputValue).map((res) => {
-    return res.item;
-  });
-  if (results.length == 0) {
-    return options;
-  }
-  return results;
-};
